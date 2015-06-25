ecc7ed72cdd1f1b8fa7b646951a2e99555ab299d
Shane Kumpf <sakserv@sakserv.org>
2015-06-25 07:53:18 -0700

    stub out reflectionutils tests

---
+++ test/main/java/com/github/sakserv/minicluster/yarn/util/ReflectionUtilsTest.java
@@ -0,0 +1,45 @@
+package com.github.sakserv.minicluster.yarn.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */public class ReflectionUtilsTest {
+
+    @Test
+    public void testGetMethodAndMakeAccessible() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFieldAndMakeAccessible() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFieldValue() throws Exception {
+
+    }
+
+    @Test
+    public void testNewDefaultInstance() throws Exception {
+
+    }
+
+    @Test
+    public void testGetInvocableConstructor() throws Exception {
+
+    }
+}
