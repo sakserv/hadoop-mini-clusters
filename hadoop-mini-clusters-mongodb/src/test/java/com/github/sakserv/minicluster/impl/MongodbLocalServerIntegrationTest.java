e93ca95ef26b959cad81fc65514df5cd7cb59c9c
Shane Kumpf <sakserv@sakserv.org>
2015-10-30 06:08:16 -0600

    ported mongodb to module

--- hadoop-mini-clusters-mongodb/src/test/java/com/github/sakserv/minicluster/impl/MongodbLocalServerIntegrationTest.java
+++ hadoop-mini-clusters-mongodb/src/test/java/com/github/sakserv/minicluster/impl/MongodbLocalServerIntegrationTest.java
@@ -15,7 +15,7 @@
 package com.github.sakserv.minicluster.impl;
 
 import com.github.sakserv.minicluster.config.ConfigVars;
-import com.github.sakserv.minicluster.config.PropertyParser;
+import com.github.sakserv.propertyparser.PropertyParser;
 import com.mongodb.*;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
