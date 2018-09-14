9b4108563efaf2d62acb6a7b432772af58093c23
Shane Kumpf <sakserv@sakserv.org>
2018-09-14 07:14:19 -0600

    clean up unused import

--- hadoop-mini-clusters-kdc/src/main/java/com/github/sakserv/minicluster/impl/KdcLocalCluster.java
+++ hadoop-mini-clusters-kdc/src/main/java/com/github/sakserv/minicluster/impl/KdcLocalCluster.java
@@ -34,7 +34,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
